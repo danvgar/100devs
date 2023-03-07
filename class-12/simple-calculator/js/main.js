42622ab9bb2946c51699ed0f6b90cd42b4ce63ca
Daniel Garcia <danvgar@gmail.com>
2023-03-06 22:08:28 -0800

    upgraded original simple calculator
    add 2 x 3 table for adding and subtracting values 1 - 3.

--- class-12/simple-calculator/js/main.js
+++ class-12/simple-calculator/js/main.js
@@ -1,26 +1,45 @@
 let total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector('#negOne').addEventListener('click', minusOne)
+document.querySelector('#one').addEventListener('click', addOne)
+document.querySelector('#negTwo').addEventListener('click', minusTwo)
+document.querySelector('#two').addEventListener('click', addTwo)
+document.querySelector('#negThree').addEventListener('click', minusThree)
+document.querySelector('#three').addEventListener('click', addThree)
+
+document.querySelector('#reset').addEventListener('click', makeZero)
+
+function minusOne() {
+  total += -1
+  document.querySelector('#placeToPutResult').innerText = total
+}
+
+function addOne() {
+  total += 1
+  document.querySelector('#placeToPutResult').innerText = total
+}
+
+function minusTwo() {
+  total += -2
+  document.querySelector('#placeToPutResult').innerText = total
+}
+
+function addTwo() {
+  total += 2
+  document.querySelector('#placeToPutResult').innerText = total
+}
+function minusThree() {
+  total += -3
+  document.querySelector('#placeToPutResult').innerText = total
+}
+
+function addThree() {
+  total += 3
+  document.querySelector('#placeToPutResult').innerText = total
+}
 
 function makeZero() {
   total = 0
   document.querySelector('#placeToPutResult').innerText = total
 }
 
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
-}
-
-function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
-}
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
-}
