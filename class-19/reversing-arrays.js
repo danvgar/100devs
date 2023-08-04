1ac59df0ca9469c7f36260db9844b39da664b5f2
Daniel Garcia <danvgar@gmail.com>
2023-08-04 07:16:52 -0700

    reverse an array and "reverse in place" without using built-in .reverse() method

--- class-19/reversing-arrays.js
+++ class-19/reversing-arrays.js
@@ -9,6 +9,36 @@
 
 // Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
 
+function reverseArray(arr) {
+    let reversedArr = []
+    for(let i = 0; i < arr.length; i++) {
+        reversedArr.push(arr[arr.length-i-1])
+    }
+    console.log(reversedArr)
+    return reversedArr
+}
+reverseArray([1,2,3])
+
+function reverseArrayInPlace(arr) {
+    // for 5 element array, index 0 replaces index 4, index 1 replaces index 3, ind
+    // if(arr.length % 2 === 1) {
+        for (let i = 0; i < arr.length; i++) {
+            // console.log(`\nCurrent: ${arr}`)
+            
+            // Copy first element at appropriate spot at back of array, decreasing index of that spot by i
+            arr.splice(arr.length - i, 0, arr[0])
+            // console.log(`First Splice: ${arr}`)
+
+            // Remove original first element
+            arr.shift()
+            // console.log(`Second Splice: ${arr}`)
+        }
+    // }
+    return arr
+}
+reverseArrayInPlace([1,2,3,4,5])
+reverseArrayInPlace([1,2,3,4,5,6])
+
 // Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
 
 // // Your code here.
@@ -19,17 +49,3 @@
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue);
 // // → [5, 4, 3, 2, 1]
-// A list
-// Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
-
-// let list = {
-//   value: 1,
-//   rest: {
-//     value: 2,
-//     rest: {
-//       value: 3,
-//       rest: null
-//     }
-//   }
-// };
-// The resulting objects form a chain, like this:
