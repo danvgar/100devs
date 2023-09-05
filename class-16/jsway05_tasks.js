3826385807473ac8307258a4fd772c16b7a52337
Daniel Garcia <danvgar@gmail.com>
2023-09-04 20:16:45 -0700

    class-16 tasks

--- class-16/jsway05_tasks.js
+++ class-16/jsway05_tasks.js
@@ -13,11 +13,18 @@
 
 // // Say hello to the user
 function sayHello(firstName, lastName) {
+  firstName = prompt("First Name:")
+  lastName = prompt("Last Name:")
   const message = `Hello, ${firstName} ${lastName}!`;
+  console.log(message)
   return message;
 }
+
+sayHello()
+
 // // TODO: ask user for first and last name
 // // TODO: call sayHello() and show its result
+
 
 // ********************
 // Number squaring
@@ -26,11 +33,13 @@ function sayHello(firstName, lastName) {
 
 // // Square the given number x
 function square1(x) {
-//   // TODO: complete the function code
+  //   // TODO: complete the function code
+  return x ** 2
 }
 
 // // Square the given number x
-const square2 = x => // TODO: complete the function code
+const square2 = x => x ** 2
+// TODO: complete the function code
 
 console.log(square1(0)); // Must show 0
 console.log(square1(2)); // Must show 4
@@ -44,20 +53,41 @@ console.log(square2(5)); // Must show 25
 // !!! info
 // Writing 10 dumb calls to `square()` is forbidden! You know how to repeat statements, don’t you? ;)
 
+
 // ********************
 // Minimum of two numbers
 // ********************
 // Let's pretend the JavaScript Math.min() function doesn’t exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+function min(num1, num2) {
+  return num1 < num2 ? num1 : num2
+}
 
 // // TODO: write the min() function
 console.log(min(4.5, 5)); // Must show 4.5
 console.log(min(19, 9));  // Must show 9
 console.log(min(1, 1));   // Must show 1
 
+
 // ********************
 // Calculator
 // ********************
 // Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
+
+function calculate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2
+    case "-":
+      return num1 - num2
+    case "*":
+      return num1 * num2
+    case "/":
+      return num1 / num2
+    default:
+      console.log("Something went wrong! Please ensure you've entered a number, an operator as a string, and another number.")
+  }
+}
 
 // // TODO: complete program
 console.log(calculate(4, "+", 6));  // Must show 10
@@ -65,10 +95,28 @@ console.log(calculate(4, "-", 6));  // Must show -2
 console.log(calculate(2, "*", 0));  // Must show 0
 console.log(calculate(12, "/", 0)); // Must show Infinity
 
+
 // ********************
 // Circumference and area of a circle
 // ********************
 // Write a program containing two functions to calculate the circumference and area of a circle defined by its radius. Test it using user input.
+
+function circleCircum(r) {
+  const PI = Math.PI
+  // circumference = 2 * pi * radius
+  return 2 * PI * r
+}
+
+circleCircum(3) // 18.849
+
+
+function circleArea(r) {
+  const PI = Math.PI
+  // area = pi * radius ** 2
+  return PI * r ** 2
+}
+
+circleArea(3) // 28.274
 
 // Here are some tips for solving this exercise:
 //  - Circumference and area calculation formulas should be part of your secondary school memories... Or a Google click away :)
