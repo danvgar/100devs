07e103749a13b5410f316196273d1b23b73bcadf
Daniel Garcia <danvgar@gmail.com>
2023-09-10 01:43:43 -0700

    class-19 live practice

--- class-19/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class-19/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,20 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
+let movieTitles = ["The Fast & The Furious", "2 Fast 2 Furious", "The Fast & The Furious: Tokyo Drift"]
+
+for(let title in movieTitles) {
+    document.querySelector("h2").innerText += `${movieTitles[title]}\n`
+}
+
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
+let numArr = [7,17,27]
+numArr = numArr.map( (num) => num + 3)
+document.querySelector("h2").innerText += `\n${numArr}\n`
+
 
 //Find the average of all the numbers from question three
+
+let avg = numArr.reduce( (acc,c) => acc + c, 0) / numArr.length
+document.querySelector('h2').innerText += `\n${avg}\n`
--- class-19/class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class-19/class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -1,20 +1,53 @@
 // *Variables*
 // Declare a variable, assign it a boolean, and alert the value
-
+let bool = true
+alert(bool)
 
 // Declare a variable, reassign it to your favorite color, and console log the value
-
+let favColor = 'blue'
+console.log(favColor)
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
+function sumDivide(n1, n2, n3, n4) {
+    return (n1 + n2 + n3) / n4
+}
 
+console.log(sumDivide(40, 30, 20, 10))
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
 
+function numPow(n1, n2) {
+    console.log(n1 ** n2)
+}
+
+numPow(3, 2)
 
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
 
+function logVsAlert(bool, str) {
+    bool ? alert(str) : console.log(str)
+}
+
+logVsAlert(true, "Hello, world!")
+
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+
+function fizzBuzz(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 5 === 0 && i % 3 === 0) {
+            console.log("fizzbuzz")
+        } else if (i % 3 === 0) {
+            console.log("fizz")
+        } else if (i % 5 === 0) {
+            console.log("buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzBuzz(15)
