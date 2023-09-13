4270e7105529fc7cce9a5ebf7f88b3748ae32164
Daniel Garcia <danvgar@gmail.com>
2023-09-12 20:37:32 -0700

    class-22 live practice

--- class-22/objects-lost-galaxy/js/main.js
+++ class-22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse = {
+    manufacturer: "Apple",
+    scrollType: "Track Pad",
+    color: "White",
+    bluetooth: true,
+
+    leftClick() {
+        return "Mouse is left-clicked."
+    },
+
+rightClick() {
+        return "Mouse is right-clicked."
+    },
+
+scroll() {
+        return "Mouse is scrolling."
+    }
+}
