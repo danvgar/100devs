6f12e5d67797338ab746b8caf390512a1197fd9d
Daniel Garcia <danvgar@gmail.com>
2023-09-13 17:26:36 -0700

    class-22 live practice

--- class-22/objects-lost-galaxy/js/main.js
+++ class-22/objects-lost-galaxy/js/main.js
@@ -1,20 +1,73 @@
 //Create a mouse object that has four properties and three methods
 
-let mouse = {
-    manufacturer: "Apple",
-    scrollType: "Track Pad",
-    color: "White",
-    bluetooth: true,
+// let mouse = {
+//     manufacturer: "Apple",
+//     scrollType: "Track Pad",
+//     color: "White",
+//     bluetooth: true,
 
-    leftClick() {
-        return "Mouse is left-clicked."
-    },
+//     leftClick() {
+//         return "Mouse is left-clicked."
+//     },
 
-rightClick() {
+//     rightClick() {
+//         return "Mouse is right-clicked."
+//     },
+
+//     scroll() {
+//         return "Mouse is scrolling."
+//     }
+// }
+
+// function MouseShop(manufacturer, scrollType, color, bluetooth){
+//     this.manufacturer = manufacturer
+//     this.scrollType = scrollType
+//     this.color = color
+//     this.bluetooth = bluetooth
+//     this.leftClick = function() {
+//         alert("Mouse is left-clicked.")
+//     }
+//     this.rightClick = function(){
+//         return "Mouse is right-clicked."
+//     }
+//     this.scroll = function(){
+//         return "Mouse is scrolling."
+//     }
+// }
+
+function MouseShop(manufacturer, scrollType, color, bluetooth){
+    this.manufacturer = manufacturer
+    this.scrollType = scrollType
+    this.color = color
+    this.bluetooth = bluetooth
+    this.leftClick = function() {
+        alert("Mouse is left-clicked.")
+    }
+    this.rightClick = function(){
         return "Mouse is right-clicked."
-    },
-
-scroll() {
+    }
+    this.scroll = function(){
         return "Mouse is scrolling."
     }
 }
+
+let mouse1 = new MouseShop("Apple", "Track Pad", "White", true)
+
+console.log(mouse1)
+
+// function MakeCar(carMake,carModel,carColor,numOfDoors){
+//     this.make = carMake
+//     this.model = carModel
+//     this.color = carColor
+//     this.doors = numOfDoors
+//     this.honk = function(){
+//       alert('BEEP BEEP FUCKER')
+//     }
+//     this.lock = function(){
+//       alert(`Locked ${this.doors} doors!`)
+//     }
+//   }
+  
+//   let hondaCivic = new MakeCar('Honda','Civic','Silver', 4)
+  
+//   let teslaRoadster = new MakeCar('Tesla','Roadster', 'Red', 2)
--- class-22/objects-street-fighter/js/main.js
+++ class-22/objects-street-fighter/js/main.js
@@ -1,1 +1,28 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+let StreetFighterCharacter = function (name, signatureMove, attackPower, taunt) {
+    this.name = name
+    this.signatureMove = signatureMove
+    this.attackPower = attackPower
+    this.taunt = taunt
+
+    this.doSignatureMove = function () {
+        alert(`${this.name} did move ${this.signatureMove}`)
+    }
+
+    this.moveLeft = function () {
+        alert(`${this.name} moved left.`)
+    }
+
+    this.moveRight = function () {
+        alert(`${this.name} moved right.`)
+    }
+
+    this.sayTaunt = function () {
+        alert(`${this.name} says ${this.taunt}`)
+    }
+}
+
+let player1 = new StreetFighterCharacter("Ryu","Shoryuken",100,`"You must love competition before you can achieve victory."`)
+
+player1.sayTaunt()
+player1.doSignatureMove()
