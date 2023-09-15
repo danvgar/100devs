be411b77139e97ebba5e4d874af3ca429575d07d
Daniel Garcia <danvgar@gmail.com>
2023-09-15 01:05:45 -0700

    class-22 live practice

--- class-22/objects-lost-galaxy/js/main.js
+++ class-22/objects-lost-galaxy/js/main.js
@@ -19,22 +19,6 @@
 //     }
 // }
 
-// function MouseShop(manufacturer, scrollType, color, bluetooth){
-//     this.manufacturer = manufacturer
-//     this.scrollType = scrollType
-//     this.color = color
-//     this.bluetooth = bluetooth
-//     this.leftClick = function() {
-//         alert("Mouse is left-clicked.")
-//     }
-//     this.rightClick = function(){
-//         return "Mouse is right-clicked."
-//     }
-//     this.scroll = function(){
-//         return "Mouse is scrolling."
-//     }
-// }
-
 function MouseShop(manufacturer, scrollType, color, bluetooth){
     this.manufacturer = manufacturer
     this.scrollType = scrollType
@@ -54,20 +38,3 @@ function MouseShop(manufacturer, scrollType, color, bluetooth){
 let mouse1 = new MouseShop("Apple", "Track Pad", "White", true)
 
 console.log(mouse1)
-
-// function MakeCar(carMake,carModel,carColor,numOfDoors){
-//     this.make = carMake
-//     this.model = carModel
-//     this.color = carColor
-//     this.doors = numOfDoors
-//     this.honk = function(){
-//       alert('BEEP BEEP FUCKER')
-//     }
-//     this.lock = function(){
-//       alert(`Locked ${this.doors} doors!`)
-//     }
-//   }
-  
-//   let hondaCivic = new MakeCar('Honda','Civic','Silver', 4)
-  
-//   let teslaRoadster = new MakeCar('Tesla','Roadster', 'Red', 2)
