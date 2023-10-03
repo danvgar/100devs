57c03cbd8307e4776f1ed0559d649065abf99533
Daniel Garcia <danvgar@gmail.com>
2023-10-03 15:33:45 -0700

    class-26 live practice

--- class-26/pokemongo_api/js/main.js
+++ class-26/pokemongo_api/js/main.js
@@ -4,6 +4,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  // https://pogoapi.net/api/v1/pokemon_encounter_data.json
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
