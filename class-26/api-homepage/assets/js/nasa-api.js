fcfd8259eaecdba31d5a778c579a8a5d8a19acaf
Daniel Garcia <danvgar@gmail.com>
2023-10-08 22:56:00 -0700

    fixed syntax errors with NASA API including key and the fact the function wasn't being called

--- class-26/api-homepage/assets/js/nasa-api.js
+++ class-26/api-homepage/assets/js/nasa-api.js
@@ -1,12 +1,11 @@
 function getNASA() {
-	// Clears out the display before pushing a new drink
-	// document.querySelector(".list").innerHTML = ""
-	
-	cocktail = document.querySelector('input').value
-	
-	fetch(`https://api.nasa.gov/planetary/apod`)
-	.then(res => res.json())
-	.then(data => {
-		console.log(data)
-	.catch(err => console.log(`Error: ${err}`))
+	fetch(`https://api.nasa.gov/planetary/apod?api_key=qGPeJOjRHwvpioGPrWZZqYFH8eMqVke4fdTuFqCJ`)
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			document.querySelector("#cocktailimage").src = data.url
+		})
+		.catch(err => console.log(`Error: ${err}`))
 }
+
+getNASA()
