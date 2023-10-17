290816201cd1f6b6489f5a71f3e2d390abc51394
Daniel Garcia <danvgar@gmail.com>
2023-10-16 23:12:55 -0700

    class-28 live practice

--- class-28/class28-materials/book-tracker/js/main.js
+++ class-28/class28-materials/book-tracker/js/main.js
@@ -1,21 +1,31 @@
 // Receive Iitle given an ISBN
 document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('h2').textContent = localStorage.getItem('books')
+
+
 
 function getFetch() {
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  
-  let ISBN = ""
+  const ISBN = document.querySelector('input').value
   // 9780980200447
-  
-  fetch(url)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    
-    const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${ISBN}&jscmd=details&format=json`
+  let url = `https://openlibrary.org/isbn/${ISBN}.json`
 
-      document.querySelector('h3').innerText = data.explanation
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      // given ISBN, console log title 
+      console.log(data)
+
+      let bookTitle = data.title
+      console.log(bookTitle)
+
+      if (!localStorage.getItem('books')) {
+        localStorage.setItem('books', bookTitle)
+      } else {
+        let books = localStorage.getItem('books') + "; " + bookTitle
+        localStorage.setItem('books', books)
+      }
+
+      document.querySelector('h2').textContent = localStorage.getItem('books')
     })
     .catch(err => {
       console.log(`error ${err}`)
