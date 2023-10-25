e977edfd8fc93796d01a3b449d9a6454bda8c13f
Daniel Garcia <danvgar@gmail.com>
2023-10-24 17:41:11 -0700

    class-30 live practice

--- class-30/class30-materials/objects-espresso-machine/js/main.js
+++ class-30/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor(model, color, waterVolume, temp) {
+        this.model = model
+        this.color = color
+        this.waterVolume = waterVolume
+        this.temp = temp
+    }
+    brew() {
+        console.log("Espresso has been brewed.")
+    }
+    heatWater() {
+        console.log("Water has been heated.")
+    }
+    getTemp() {
+        console.log(`Current temp is ${temp}`)
+    }
+}
+
+let gaggiaClassic = new EspressoMachine("Classic Pro", "white", 2, 50)
