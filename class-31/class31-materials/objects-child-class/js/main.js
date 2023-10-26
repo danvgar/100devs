2f9654e6880e5068e8c1e4371d1f0a32c8a16051
Daniel Garcia <danvgar@gmail.com>
2023-10-25 22:02:08 -0700

    class-31 live practice

--- class-31/class31-materials/objects-child-class/js/main.js
+++ class-31/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,20 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Pokemon {
+    constructor(species, type1, cry) {
+        this.species = species
+        this.type1 = type1
+    }
+    speak() {
+        console.log(`${cry}`)
+    }
+}
+
+class Swampert extends Pokemon {
+    constructor(species, nickname, type1, type2) {
+        super(species)
+        this.nickname = nickname
+        super(type1)
+        this.type2 = type2
+    }
+}
