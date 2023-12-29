83f479bd1e2d32dc3444eaf15e1752c947870ddf
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2023-12-28 16:28:10 -0800

    small progress on basic full stack CRUD app

--- class-39/server.js
+++ class-39/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 8000;
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}, better go catch it!`))
