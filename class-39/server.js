46fde35330fb744c6f280610583665a21cca320c
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2023-12-29 17:29:43 -0800

    updated server to use EJS, and added index.ejs to views folder.

--- class-39/server.js
+++ class-39/server.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const cors = require('cors')
-
 const app = express();
-
-const PORT = 8000;
 
 // ========================
 // Link to Database
 // ========================
 // Updates environment variables
 // @see https://zellwk.com/blog/environment-variables/
-const dotenv = require('dotenv').config({
+require('dotenv').config({
     path: 'secrets/variables.env'
 })
-
-// Replace process.env.DB_URL with your actual connection string
 const connectionString = process.env.DB_URL
 // MongoDB connection string removed in favor of environment variables. Will not bother purging old commits because this database is a test database anyway and will promptly be deleted.
 
@@ -24,41 +18,38 @@ const connectionString = process.env.DB_URL
 MongoClient.connect(connectionString)
     // If connection promise fulfilled, continue
     .then(client => {
+        console.log('Connected to Database')
+        // Create a new MongoDB Database
+        const db = client.db('test-db')
+        // Create a new MongoDB Collection within the Database
+        const quotesCollection = db.collection('quotes')
 
         // ========================
         // Middlewares
         // ========================
+        // Use bodyParser to handle form submissions
+        app.use(bodyParser.urlencoded({ extended: true }))
+
+        // Tell express that we will be using EJS as the template engine
+        app.set('view engine', 'ejs')
 
         // ========================
         // Routes
         // ========================
 
-        console.log('Connected to Database')
-
-        // Create a new MongoDB Database
-        const db = client.db('test-db')
-
-        // Create a new MongoDB Collection within the Database
-        const quotesCollection = db.collection('quotes')
-
-        // Use CORS within server
-        app.use(cors())
-
-        // Use bodyParser to handle form submissions
-        app.use(bodyParser.urlencoded({ extended: true }))
-
+        // GET
         app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
             db.collection('quotes')
                 .find()
                 .toArray()
                 .then(results => {
                     console.log(results)
+                    res.render('index.ejs', { quotes: results })
                 })
-                .catch(error => console.error(error))
-            // ...
+                .catch(error => console.error(error));
         })
 
+        // PUT
         // Upon form submission at <form action="/quotes">
         app.post('/quotes', (req, res) => {
             // Working specifically with our MongoDB Collection called quotesCollection
@@ -73,10 +64,13 @@ MongoClient.connect(connectionString)
                 // If post promise rejected, console error.
                 .catch(error => console.error(error))
         })
+        // POST
+        // DELETE
 
         // ========================
         // Listen
         // ========================
+        const PORT = 8000;
         app.listen(PORT, () => console.log(`Your server is running on port ${PORT}, better go catch it!`))
     })
     // If connection promise rejected, console error.
